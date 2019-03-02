776c3c0311b5700347252862cd82d17e54b3faf2
planetsp <triplefinn@gmail.com>
2019-03-02 13:10:08 -0500

    Update database-control.service.ts

--- src/app/core/database-control.service.ts
+++ src/app/core/database-control.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
+export interface Team {
+
+}
+export interface Event {
+
+}
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseControlService {
 
   constructor() { }
+  public createTeam(){
+
+  }
+  /**
+   * name
+   */
+  public createEvent() {
+    
+  }
 }
