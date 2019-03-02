42023eb401cd12dcb0ea4d24383dd38f73ddcaf0
Ti-ruo Yan <yantiruo@gmail.com>
2019-03-02 14:26:38 -0500

    puishhhhh

--- src/app/event/event-create/event-create.component.ts
+++ src/app/event/event-create/event-create.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Event, Team } from '../../core/database-control.service'
+
 @Component({
   selector: 'app-event-create',
   templateUrl: './event-create.component.html',
   styleUrls: ['./event-create.component.css']
 })
 export class EventCreateComponent implements OnInit {
-
+  reqs: Map<string, string>;
+  currEvent: Event = {
+    name: '',
+    organizer: '',
+    minTeamMembers: 0,
+    maxTeamMembers: 100,
+    time: null,
+    place: '',
+    teams: null,
+  }
   constructor() { }
 
   ngOnInit() {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from '../app-routing.module';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatFormFieldModule, MatSelectModule, MatInputModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatNativeDateModule } from '@angular/material';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSliderModule} from '@angular/material/slider';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [],
   exports: [
@@ -17,6 +21,11 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSliderModule,
+    FormsModule,
   ]
 })
 export class SharedModule { }
